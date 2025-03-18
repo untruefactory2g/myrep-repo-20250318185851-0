28ae2f93feca212063ff6b1b20667d83af69efaf
untruefactory2g <untrue-factory.2g@icloud.com>
2025-03-18 23:57:05 +0400

    Update script_0.py

--- script_0.py
+++ script_0.py
@@ -1,5 +1,4 @@
-16
-print(f"Performing blockchain info check: w3.eth.get_block_number()")
+17print(f"Performing blockchain info check: w3.eth.get_block_number()")
 print(f"Web3 is connected: {w3.is_connected}")
 print(f"Commit Number: 0")
 print(f"Random number: 37")
